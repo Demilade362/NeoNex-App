8ff5c3292e4aa6c9417bec9ccab8ba6448f80b9a
Demilade362 <ademolademilade362@gmail.com>
2024-07-02 16:40:13 +0100

    airdrop form

--- app/page.jsx
+++ app/page.jsx
@@ -70,11 +70,25 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <section className="my-10">
+      <section className="my-20">
         <div className="container">
-          <h1 className="text-2xl text-center">
+          <h1 className="text-2xl mb-7 text-center">
             Participate in Airdrop
           </h1>
+
+
+          <form class="max-w-md mx-auto">
+            <div class="mb-5">
+              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+              <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required />
+            </div>
+            <div class="mb-5">
+              <label for="Wallet" class="block mb-2 text-sm font-medium text-gray-900">Your Wallet Address</label>
+              <input type="test" id="Wallet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+            </div>
+            <button type="submit" class="text-white bg-green-400 hover:bg-green-500 w-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit Wallet Address</button>
+          </form>
+
         </div>
       </section>
     </>
