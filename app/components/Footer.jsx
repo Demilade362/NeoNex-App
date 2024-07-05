176bd5ee48bb6960f30e7908ea8296e45af9fc74
Demilade362 <ademolademilade362@gmail.com>
2024-07-05 11:20:05 +0100

    made new changes to welcome page

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,13 +12,13 @@ export default function Footer() {
                     </div>
                     <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
-                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Quick Communication</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
-                                    <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                                    <a href="/" class="hover:underline">Telegram</a>
                                 </li>
                                 <li>
-                                    <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                                    <a href="/" class="hover:underline">Whatsapp</a>
                                 </li>
                             </ul>
                         </div>
@@ -26,10 +26,10 @@ export default function Footer() {
                             <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
                             <ul class="text-gray-500 dark:text-gray-400 font-medium">
                                 <li class="mb-4">
-                                    <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                                    <a href="/" class="hover:underline ">Twitter</a>
                                 </li>
                                 <li>
-                                    <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                                    <a href="/" class="hover:underline">Discord</a>
                                 </li>
                             </ul>
                         </div>
@@ -48,7 +48,7 @@ export default function Footer() {
                 </div>
                 <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div class="sm:flex sm:items-center sm:justify-between">
-                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">NeoNex</a>. All Rights Reserved.
                     </span>
                     <div class="flex mt-4 sm:justify-center sm:mt-0">
                         <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- app/page.jsx
+++ app/page.jsx
@@ -71,24 +71,44 @@ export default function Home() {
         </div>
       </div>
       <section className="my-20">
-        <div className="container">
-          <h1 className="text-2xl mb-7 text-center">
-            Participate in Airdrop
-          </h1>
-
-
-          <form class="max-w-md mx-auto">
-            <div class="mb-5">
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-              <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@email.com" required />
+        <div className="container mx-auto flex flex-col lg:flex-row justify-center gap-6 ">
+          <div className="grow">
+            <h1 className="text-2xl mb-7 text-center">
+              Participate in Airdrop
+            </h1>
+            <form>
+              <div class="mb-5">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@email.com" required />
+              </div>
+              <div class="mb-5">
+                <label for="Wallet" class="block mb-2 text-sm font-medium text-gray-900">Your Wallet Address</label>
+                <input type="test" id="Wallet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+              </div>
+              <button type="submit" class="text-white bg-green-400 hover:bg-green-500 w-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit Wallet Address</button>
+            </form>
+          </div>
+          <div className="grow">
+            <h1 className="text-center text-3xl mb-9">Tokenomics</h1>
+            <div id="fullWidthTabContent">
+              <div class="p-4 bg-white rounded-lg md:p-8">
+                <dl class="flex lg:flex-row mx-auto gap-20 md:flex-col justify-center">
+                  <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">6</dt>
+                    <dd class="text-gray-500 dark:text-gray-400">Team</dd>
+                  </div>
+                  <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">1B</dt>
+                    <dd class="text-gray-500 dark:text-gray-400">Total Token</dd>
+                  </div>
+                  <div class="flex flex-col items-center justify-center">
+                    <dt class="mb-2 text-3xl font-extrabold">0.0006$</dt>
+                    <dd class="text-gray-500 dark:text-gray-400">Token Price</dd>
+                  </div>
+                </dl>
+              </div>
             </div>
-            <div class="mb-5">
-              <label for="Wallet" class="block mb-2 text-sm font-medium text-gray-900">Your Wallet Address</label>
-              <input type="test" id="Wallet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-            </div>
-            <button type="submit" class="text-white bg-green-400 hover:bg-green-500 w-full focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit Wallet Address</button>
-          </form>
-
+          </div>
         </div>
       </section>
     </>
